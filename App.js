e42a6f168eda38e7501b9fb12240fc40e952a2f9
MZHAR QURESHI <143028768+Mazhar7867@users.noreply.github.com>
2024-11-19 11:40:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,128 @@
+import React from 'react'
+import NavBaarTop from './V-Mart/NavbaarTop'
+import MainSection from './V-Mart/MainSection'
+import MainSectionMain from './V-Mart/MainSectionMain'
+function App() {
+  return (
+    <div>
+      <NavBaarTop />
+
+      <div className='bg-gray-50 '>
+
+      
+      <div className=' ml-[10%] mr-[10%]  '>
+      <MainSection/>
+      <div className='grid grid-cols-5 gap-4 shadow-sm border-gray-400 bg-none'>
+
+      <MainSectionMain imgUrl="https://falseeyelashes.co.uk/cdn/shop/files/essence-bye-bye-panda-eyes-smudge-proof-volumizing-and-defining-mascara-1.jpg?v=1718895433" name="  Beauty" information="Essence Mascara" rating="4.94" price="$9.45" />
+      <MainSectionMain imgUrl="https://falseeyelashes.co.uk/cdn/shop/files/essence-bye-bye-panda-eyes-smudge-proof-volumizing-and-defining-mascara-1.jpg?v=1718895433" name="  Beauty" information="Eyeshadow Palette" rating="3.29" price="$19.99" />
+      <MainSectionMain imgUrl="https://falseeyelashes.co.uk/cdn/shop/files/essence-bye-bye-panda-eyes-smudge-proof-volumizing-and-defining-mascara-1.jpg?v=1718895433" name="  Beauty" information="Powder Canister" rating="3.82" price="$14.99" />
+      <MainSectionMain imgUrl="https://falseeyelashes.co.uk/cdn/shop/files/essence-bye-bye-panda-eyes-smudge-proof-volumizing-and-defining-mascara-1.jpg?v=1718895433" name="  Beauty" information="Red Nail Polish" rating="3.91" price="$8.99" />
+      <MainSectionMain imgUrl="https://falseeyelashes.co.uk/cdn/shop/files/essence-bye-bye-panda-eyes-smudge-proof-volumizing-and-defining-mascara-1.jpg?v=1718895433" name="  Beauty" information="Red Lipstick" rating="2.29" price="$12.99" />
+     
+      <MainSectionMain imgUrl="https://c0.wallpaperflare.com/preview/52/620/29/perfume-bottle-isolated-white.jpg" name="  Fragrances" information="Calvin Kelien" rating="3.4" price="$7.63" />
+      <MainSectionMain imgUrl="https://c0.wallpaperflare.com/preview/52/620/29/perfume-bottle-isolated-white.jpg" name="  Fragrances" information="Dolce Shine Eau" rating="5.91" price="$4.97" />
+      <MainSectionMain imgUrl="https://c0.wallpaperflare.com/preview/52/620/29/perfume-bottle-isolated-white.jpg" name="  Fragrances" information="Chanel Coco " rating="4.44" price="$6.64" />
+      <MainSectionMain imgUrl="https://c0.wallpaperflare.com/preview/52/620/29/perfume-bottle-isolated-white.jpg" name="  Fragrances" information="Dior J'adore" rating="4.33" price="$3.74" />
+      <MainSectionMain imgUrl="https://c0.wallpaperflare.com/preview/52/620/29/perfume-bottle-isolated-white.jpg" name="  Fragrances" information="Gucci Blom Eau" rating="3.61" price="$6.69" />
+      
+      <MainSectionMain imgUrl="https://m.media-amazon.com/images/I/71H5D-G6BhL._AC_UL480_FMwebp_QL65_.jpg" name="  Furniture" information="Annihale colombo bed" rating="3.91" price="$8.24" />
+      <MainSectionMain imgUrl="https://m.media-amazon.com/images/I/715gowTpy8L._AC_UL480_FMwebp_QL65_.jpg" name="  Furniture" information="Annihal colombo sofa" rating="5.61" price="$4.49" />
+      <MainSectionMain imgUrl="https://m.media-amazon.com/images/I/719Ka1FkXNL._AC_UL480_FMwebp_QL65_.jpg" name="  Furniture" information="Beside Table African" rating="3.74" price="$4.53" />
+      <MainSectionMain imgUrl="https://m.media-amazon.com/images/I/715gowTpy8L._AC_UL480_FMwebp_QL65_.jpg" name="  Furniture" information="Knoil Sarrien Executive" rating="6.36" price="$2.25" />
+      <MainSectionMain imgUrl="https://m.media-amazon.com/images/I/715gowTpy8L._AC_UL480_FMwebp_QL65_.jpg" name="  Furniture" information="Wooden Bathroom Sink" rating="3.53" price="$4.63" />
+      </div>
+     </div>
+       </div>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import Phonebook from './Phonebook';
+// import SingleContact from './SingleContact';
+
+
+// function App() {
+// const router = createBrowserRouter([
+// {
+// path:"/information",
+// element: <P   honebook/>,
+// children:[
+//   {
+//     path:":userName"
+//   }
+// ]
+// },
+// {
+// path:"/information/:userName",
+// element: <SingleContact/>
+// },
+
+// ]);
+
+// return(
+// <>
+// <RouterProvider router={router}/>
+
+// </>
+// )
+// }
+
+// export default App
